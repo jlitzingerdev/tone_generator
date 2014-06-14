637bf2d00497a886f076720414100b907b9838d0
Jason Litzinger <jlitzingerdev@gmail.com>
2014-06-14 17:33:35 -0600

    Working tone generator, plays with GStreamer.

--- tone_generator.go
+++ tone_generator.go
@@ -9,56 +9,50 @@ import (
 	"fmt"
 	"bytes"
 	"encoding/binary"
-	//"math"
+	"math"
+	"io/ioutil"
 )
 
-const WAV_HDR_LEN = 44
-
-func buildWavHeader(buf *bytes.Buffer, bitDepth, channels uint16, sampleRate, dataSize uint32) {
+func buildWavHeader(buf *bytes.Buffer, bitDepth, channels, sampleRate, dataSize int) {
 
 	binary.Write(buf, binary.BigEndian, []byte{'R','I','F','F'})
 	binary.Write(buf, binary.LittleEndian, uint32(36 + dataSize))
 	binary.Write(buf, binary.BigEndian, []byte{'W','A','V','E'})
 	binary.Write(buf, binary.BigEndian, []byte{'f','m','t',' '})
 	binary.Write(buf, binary.LittleEndian, uint32(16))
-	binary.Write(buf, binary.LittleEndian, uint16(16))
-	binary.Write(buf, binary.LittleEndian, channels)
-	binary.Write(buf, binary.LittleEndian, sampleRate)
+	binary.Write(buf, binary.LittleEndian, uint16(1))
+	binary.Write(buf, binary.LittleEndian, uint16(channels))
+	binary.Write(buf, binary.LittleEndian, uint32(sampleRate))
 
 	//ByteRate
-	binary.Write(buf, binary.LittleEndian, uint32(sampleRate * uint32(channels) * uint32(bitDepth / 8)))
-	binary.Write(buf, binary.LittleEndian, uint32(channels * (bitDepth / 8)))
-	binary.Write(buf, binary.LittleEndian, bitDepth)
+	binary.Write(buf, binary.LittleEndian, uint32(sampleRate * channels * (bitDepth / 8)))
+	binary.Write(buf, binary.LittleEndian, uint16(channels * (bitDepth / 8)))
+	binary.Write(buf, binary.LittleEndian, uint16(bitDepth))
 	binary.Write(buf, binary.BigEndian, []byte{'d','a','t','a'})
-	binary.Write(buf, binary.LittleEndian, dataSize)
-	fmt.Printf("%X\n", buf)
+	binary.Write(buf, binary.LittleEndian, uint32(dataSize))
 }
 
 func main () {
-	//freq := flag.Int("f", 1, "Tone frequency.")
+	freq := flag.Int("f", 1000, "Tone frequency.")
 	rate := flag.Int("r", 8000, "Sample rate")
 	flag.Parse()
-	//var duration uint32 = 5
-	var numChannels uint16 = 1
-	var bitDepth uint16 = 16
+	duration := 1
+	numChannels := 1
+	bitDepth := 16
 
 	fmt.Printf("Tone Generator\n")
-	//amplitude := 0.0
-	sampleRate := uint32(*rate)
-	increment := 1.0 / float64(sampleRate)
-	fmt.Printf("increment=%f\n", increment)
+	amplitude := 0.0
+	sampleRate := *rate
 
 	buf := new(bytes.Buffer)
-	//numSamples := sampleRate * duration * uint32(numChannels);
-	//data := make([]byte, numSamples)
-	var dataSize uint32 = sampleRate * uint32(numChannels) * uint32(bitDepth / 8)
+	numSamples := sampleRate * duration * int(numChannels);
+	dataSize  := sampleRate * numChannels * (bitDepth / 8)
 	buildWavHeader(buf, bitDepth, numChannels, sampleRate, dataSize)
-/*
-	for t := 0.0; t < 5.0; t += increment {
-		fmt.Printf("t=%f, ", t)
-		amplitude = math.Sin(2 * math.Pi * float64(*freq) * t)
-		fmt.Printf("amplitude=%f\n", amplitude);
+
+	for i := 0; i < numSamples; i++ {
+		amplitude = 32767.0 * math.Sin(2 * math.Pi * float64(*freq) * (float64(i) / float64(sampleRate)))
+		binary.Write(buf, binary.LittleEndian, int16(amplitude))
 	}
-*/
+	ioutil.WriteFile("tone.wav", buf.Bytes(), 0666)
 }
 
