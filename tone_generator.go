202455efa215f2f5b75c6d93d083412a7c4a456a
Jason Litzinger <jlitzingerdev@gmail.com>
2014-06-14 09:16:02 -0600

    Header building should be complete.

--- tone_generator.go
+++ tone_generator.go
@@ -7,37 +7,52 @@ package main
 import (
 	"flag"
 	"fmt"
+	"bytes"
+	"encoding/binary"
 	//"math"
 )
 
 const WAV_HDR_LEN = 44
 
-func buildWavHeader(sampleRate, bitDepth, channels, numSamples int) {
-	riff := []byte{'R','I','F','F'}
-	//format := []byte{'W','A','V','E'}
-	//dataSize := sampleRate * channels * (bitDepth / 8)
-	hdr := make([]byte, WAV_HDR_LEN)
-	i := 0
-	i += copy(hdr,riff)
-	fmt.Printf("%X\n", hdr)
-	fmt.Printf("%d\n", i)
+func buildWavHeader(buf *bytes.Buffer, bitDepth, channels uint16, sampleRate, dataSize uint32) {
+
+	binary.Write(buf, binary.BigEndian, []byte{'R','I','F','F'})
+	binary.Write(buf, binary.LittleEndian, uint32(36 + dataSize))
+	binary.Write(buf, binary.BigEndian, []byte{'W','A','V','E'})
+	binary.Write(buf, binary.BigEndian, []byte{'f','m','t',' '})
+	binary.Write(buf, binary.LittleEndian, uint32(16))
+	binary.Write(buf, binary.LittleEndian, uint16(16))
+	binary.Write(buf, binary.LittleEndian, channels)
+	binary.Write(buf, binary.LittleEndian, sampleRate)
+
+	//ByteRate
+	binary.Write(buf, binary.LittleEndian, uint32(sampleRate * uint32(channels) * uint32(bitDepth / 8)))
+	binary.Write(buf, binary.LittleEndian, uint32(channels * (bitDepth / 8)))
+	binary.Write(buf, binary.LittleEndian, bitDepth)
+	binary.Write(buf, binary.BigEndian, []byte{'d','a','t','a'})
+	binary.Write(buf, binary.LittleEndian, dataSize)
+	fmt.Printf("%X\n", buf)
 }
 
 func main () {
 	//freq := flag.Int("f", 1, "Tone frequency.")
 	rate := flag.Int("r", 8000, "Sample rate")
 	flag.Parse()
-	duration := 5
-	numChannels := 1
+	//var duration uint32 = 5
+	var numChannels uint16 = 1
+	var bitDepth uint16 = 16
 
 	fmt.Printf("Tone Generator\n")
 	//amplitude := 0.0
-	sampleRate := int(*rate)
+	sampleRate := uint32(*rate)
 	increment := 1.0 / float64(sampleRate)
 	fmt.Printf("increment=%f\n", increment)
 
-	numSamples := sampleRate * duration * numChannels;
-	buildWavHeader(sampleRate, 16, numChannels, numSamples)
+	buf := new(bytes.Buffer)
+	//numSamples := sampleRate * duration * uint32(numChannels);
+	//data := make([]byte, numSamples)
+	var dataSize uint32 = sampleRate * uint32(numChannels) * uint32(bitDepth / 8)
+	buildWavHeader(buf, bitDepth, numChannels, sampleRate, dataSize)
 /*
 	for t := 0.0; t < 5.0; t += increment {
 		fmt.Printf("t=%f, ", t)
